34016736976f7d5e21eb37119db9139a33927c87
RealVolAnd <out_of_bound@yahoo.com>
2021-08-20 20:29:29 +0300

    Dynamyc shadow with accelerator sensor 3

--- app/src/main/java/net/smartgekko/slidinglayers/MainActivity.kt
+++ app/src/main/java/net/smartgekko/slidinglayers/MainActivity.kt
@@ -13,8 +13,8 @@ import java.lang.Math.abs
 
 
 class MainActivity : AppCompatActivity() {
-    val SHADOW_SIZE_LIMIT_X = 2F
-    val SHADOW_SIZE_LIMIT_Y = 4F
+    val SHADOW_SIZE_LIMIT_X = 8F
+    val SHADOW_SIZE_LIMIT_Y = 8F
     lateinit var textView: TextView
     var sensorManager: SensorManager? = null
     var sensorMove: Sensor? = null
@@ -39,10 +39,8 @@ class MainActivity : AppCompatActivity() {
     var listenerMove: SensorEventListener = object : SensorEventListener {
         override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
         override fun onSensorChanged(event: SensorEvent) {
-            valueX = event.values[0]
-            if(abs(valueX)>=SHADOW_SIZE_LIMIT_X) valueX = 0+SHADOW_SIZE_LIMIT_X*(valueX/abs(valueX))
-            valueY = event.values[1]
-            if(abs(valueY)>=SHADOW_SIZE_LIMIT_Y) valueY = 0+SHADOW_SIZE_LIMIT_Y*(valueY/abs(valueY))
+            valueX = 0+event.values[0]*(SHADOW_SIZE_LIMIT_X/10)
+            valueY = 0+event.values[1]*(SHADOW_SIZE_LIMIT_Y/10)
             valueZ = event.values[2]
             textView.setText("Shadow")
             setTextShadow(valueX,valueY)
@@ -51,7 +49,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setTextShadow(x: Float,y: Float,limit: Float=5F){
 
-            textView.setShadowLayer(10F,0-x,y,getColor(R.color.gray))
+            textView.setShadowLayer(5F,0-x,y,getColor(R.color.gray))
 
 
     }
